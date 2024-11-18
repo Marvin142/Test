daf00e611051664b662e3dce71c5b5ff0c4e19d9
Marvin142 Sippel <sippelmarvin72@gmail.com>
2024-11-18 09:30:03 +0000

    Task 7 Has Won

--- app/app/App/Http/Controllers/GameController.php
+++ app/app/App/Http/Controllers/GameController.php
@@ -40,7 +40,7 @@ class GameController extends Controller
      * @throws Exception
      */
     protected function someoneHasWon( GameBoard $game ): bool {
-        // ##### TASK 7 - Make this check more efficient ###############################################################
+        // ##### TASK 8 - Make this check more efficient ###############################################################
         // =============================================================================================================
         // This function checks if the game has already won. It does this by checking for every possible winning
         // condition. For example, the first block below checks if the first row contains identical marks that are not
@@ -102,14 +102,21 @@ class GameController extends Controller
     }
 
     protected function whoHasWon( GameBoard $game ): ?GamePlayer {
-        // ##### TASK 8 - Check who has won ############################################################################
+        // ##### TASK 7 - Check who has won ############################################################################
         // =============================================================================================================
         // Here, you need to code a way to find out who has won the game.
         // This function needs to return null if nobody has won yet - you can use someoneHasWon( $game ) for this.
         // If someone has won, it needs to return either GamePlayer::Human or GamePlayer::Robot.
         // =============================================================================================================
-
-        return null;
+        //If there is a Player
+        if (someoneHasWon( $game )) {
+            return GamePlayer::Human || GamePlayer::Robot;
+        }
+        // If there is no Player
+        else {
+            return null;
+        }
+        
     }
 
     /**
@@ -181,7 +188,7 @@ class GameController extends Controller
         // You can compare two values with
         // $a === $b       gets true if $a is equals $b
         // $a !== $b       gets true if $a is not equals $b
-        if ($game->getSpace($a !== $b)) {
+        if ($game->getSpace($x !== $y)) {
         // Once all the checks have passed, you can finally update the game board by calling
          $game->setSpace( $x, $y, GameMark::Circle );
         }
